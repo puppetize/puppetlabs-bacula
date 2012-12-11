f5095d3377b789c074c50291a6bb715eb961da59
Uwe Stuehler <uwe@bsdx.de>
2012-12-11 10:55:25 +0100

    job definition now takes a template parameter
    The template is evaluated within the context of the job definition.

--- manifests/director/job.pp
+++ manifests/director/job.pp
@@ -1,7 +1,7 @@
 # Define a backup or restore Job.
 define bacula::director::job($comment, $client, $fileset, $accurate = false,
   $jobdefs = '', $type = '', $messages = '', $level = '', $content = '',
-  $storage = '', $pool = '', $where = '')
+  $storage = '', $pool = '', $where = '', $template = '')
 {
   bacula::director::config_fragment { "job/${name}":
     content => template('bacula/bacula-dir.conf/job.erb')
